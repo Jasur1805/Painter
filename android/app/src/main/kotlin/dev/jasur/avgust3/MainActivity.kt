2c5a93bfbc9ae7a683b863c4818ea363c2f2f5c6
jasur <abdumannopovj147@gmail.com>
2023-08-04 17:17:02 +0500

    first commit

---
+++ android/app/src/main/kotlin/dev/jasur/avgust3/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.jasur.avgust3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/main.dart
@@ -0,0 +1,20 @@
+import 'package:avgust3/src/screens/widget/home_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+      );
+}
